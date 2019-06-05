ad857450ba56bf993fd9bfe83eae2ae095b02ecd
Juan David OSPINA SALAS <uospiju@ile.mtl.qc.ca>
2019-06-05 15:21:54 -0400

    userModule added. organizing exports/imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { EventsAppComponent } from './events-app.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
-import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/shared/events-list-resolver.service';
+
+import {
+  EventDetailsComponent,
+  EventListResolver,
+  EventRouteActivator,
+  EventService,
+  EventThumbnailComponent,
+  CreateEventComponent,
+  EventsListComponent
+} from './events/index';
 
 @NgModule({
   declarations: [
